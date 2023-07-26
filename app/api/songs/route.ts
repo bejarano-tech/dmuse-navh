bb334a6e33f2fa9142daa8714e37de1446b33b22
Frederick Bejarano Sanchez <bejarano.frederick@gmail.com>
2023-07-26 12:01:23 -0500

    fix order and add revalidate

--- app/api/songs/route.ts
+++ app/api/songs/route.ts
@@ -2,6 +2,6 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from 'next/server'
  
 export async function GET() {
-  const { rows } = await sql`SELECT * FROM song;`;
+  const { rows } = await sql`SELECT * FROM song ORDER BY name;`;
   return NextResponse.json(rows)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,20 @@
 import Image from 'next/image'
 import { headers } from 'next/headers'
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from 'react';
 
 export default async function Home() {
   const songs = await getSongs()
 
-  console.log(songs)
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {songs.map((song: { id: any; name: string; attributes: any[]; image: string; }) => <a
+        {songs.map((song: { id: any; name: string; attributes: any[]; image: string; external_url: string; }) => <a
           key={`song-${song.id}-${song.name}`}
-          href=""
+          href={song.external_url}
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
           rel="noopener noreferrer"
         >
           <Image width={250} height={250} src={song.image} alt={`${song.name} Cover Image`}/>
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`my-3 text-2xl font-semibold`}>
             {song.name}
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -33,6 +29,6 @@ export default async function Home() {
 async function getSongs() {
   const host = headers().get("host");
   const protocol = process?.env.NODE_ENV==="development"?"http":"https"
-  const res = await fetch(`${protocol}://${host}/api/songs`)
+  const res = await fetch(`${protocol}://${host}/api/songs`, {next: { revalidate: 60 }})
   return res.json()
 }
